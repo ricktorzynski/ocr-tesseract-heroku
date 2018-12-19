4d539fd314c361393d740c9439c0a0d2658d5169
Rick Torzynski <ricktorzynski@gmail.com>
2018-12-19 09:17:52 -0500

    fixing upload path

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ __source__ = ''
 app = Flask(__name__)
 
 MYDIR = os.path.dirname(__file__)
-UPLOAD_FOLDER = '/tmp'
+UPLOAD_FOLDER = 'tmp'
 app.config['UPLOAD_FOLDER'] = os.path.join(MYDIR + "/" + UPLOAD_FOLDER)
 
 flask.Provider(app, '9xFpI8E9NfSpMm0T9J0A').attach()
